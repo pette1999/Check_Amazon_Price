e99fa333cd5823888408ef62b2a4d8d865aa2a96
Peter Chen <haichen@chapman.edu>
2020-07-15 20:28:50 -0700

    fix a bug

--- script.py
+++ script.py
@@ -11,8 +11,7 @@ def check_price():
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    #title = soup.find(id="productTitle").get_text()
-    price = soup.find(id="priceblock_ourprice").text
+    price = soup.find(id="priceblock_ourprice").get_text()
     converted_price = float(price[1:].strip())
     
     print(converted_price)
